494337b2a852ada2bdfa12c91bea9b3c52206201
Sumanth0802 <110766877+Sumanth0802@users.noreply.github.com>
2024-02-20 14:15:52 +0530

    1st commit

---
+++ src/test/java/dev/jaswanth/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jaswanth.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
